f7a32ec7fb8c462a3df5d90643be11414a6c7bcf
丁勇 <1052717219@qq.com>
2024-09-06 17:58:01 +0800

    提交

--- src/main/java/cn/petmanagementsystem/PetManagementSystemApplication.java
+++ src/main/java/cn/petmanagementsystem/PetManagementSystemApplication.java
@@ -1,13 +1,15 @@
 package cn.petmanagementsystem;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class PetManagementSystemApplication {
+public class PetManagementSystemApplication extends SpringBootServletInitializer {
 
-    public static void main(String[] args) {
-        SpringApplication.run(PetManagementSystemApplication.class, args);
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(PetManagementSystemApplication.class);
     }
 
 }
